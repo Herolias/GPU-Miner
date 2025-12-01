bf0d9cc9e7ea5a3a5928653b17a687f648bb7136
Elias Rehfus <esuperfus@gmail.com>
2025-12-01 22:49:40 +0100

    welcome script for docker

---
+++ scripts/welcome.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo -e "\033[1;32mGPU Miner successfully installed!\033[0m"
+echo "To start the miner, run:"
+echo -e "\033[1;33mpython main.py --cpu --workers x\033[0m"
+echo "Replace x with the number of CPU workers to be active."
+echo "1 per CPU core and GB of RAM is a good estimate."
